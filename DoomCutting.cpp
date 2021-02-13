57a0397d0f2447061d57f4ef16cd6037bfc0bb5f
kimbokChi <han15050424@gmail.com>
2021-02-13 09:00:14 +0900

    새로운 이펙트인, DoomCutting을 추가한다
    추가한 이펙트는 MostimaBoom과 마찬가지로 컴포넌트 형식의 이펙트이다.

---
+++ DoomCutting.cpp
@@ -0,0 +1,92 @@
+#include "DXUT.h"
+#include "DoomCutting.h"
+
+void DoomCutting::Init()
+{
+	m_Renderer = Owner->TryAddComponent<Renderer>();
+	m_Renderer->Image = IMAGE->AddImage("Doom", "./Assets/Particle/Doom/Doom.png");
+	m_Renderer->IsEnable = false;
+
+	m_CutTimer = new Timer(0.05f, false);
+	m_CutTimer->Start(0.1f, 16);
+
+	m_HexagonLight = IMAGE->AddImage("Hexagon_Light", "./Assets/Particle/Doom/Hexagon_Light.png");
+}
+
+void DoomCutting::Update()
+{
+	m_CutTimer->Update();
+
+	if (m_CutTimer->IsOver())
+	{
+		float rot = RandomRadian();
+		float length = 2.00f;
+		float speed  = 1.75f;
+
+		Vector2 position = (Vector2(cosf(rot), sinf(rot)) * 36) + Owner->GetTransform()->Position;
+
+		Particle* particle = PARTICLE->CreateParticle(ParticleName::CutEffect, true, position, length);
+		particle->GetRenderer()->Image = m_HexagonLight;
+		particle->GetTransform()->Rotation = RandomRadian();
+		particle->Speed *= speed;
+
+		Particle* particle2 = PARTICLE->CreateParticle(ParticleName::CutEffect, true, position, length);
+		particle2->GetTransform()->Rotation = particle->GetTransform()->Rotation;
+		particle2->Speed *= speed;
+
+		if (m_CutTimer->InvokePercent() >= 1)
+		{
+			float scale = 3.5f;
+			float rots[12];
+			Vector2 positions[12];
+
+			CAMERA->Shake(45, 0.5f);
+			m_Renderer->IsEnable = true;
+
+			speed  = 0.4f;
+			length = 2.0f;
+
+			for (int i = 0; i < 12; i++)
+			{
+				rots[i] = RandomRadian();
+				positions[i] = Vector2(D3DXToDegree(cosf(rots[i])), D3DXToDegree(sinf(rots[i]))) * 3.0f;
+				positions[i] += Owner->GetTransform()->Position;
+
+				Particle* particle = PARTICLE->CreateParticle(ParticleName::CutEffect, true, positions[i], length);
+				particle->GetRenderer()->Image = m_HexagonLight;
+
+				particle->Speed *= speed;
+				particle->GetTransform()->Rotation = rots[i];
+				particle->GetTransform()->Scale *= scale;
+			}
+			for (int i = 0; i < 12; i++)
+			{
+				Particle* particle = PARTICLE->CreateParticle(ParticleName::CutEffect, true, positions[i], length);
+				particle->Speed *= speed;
+				particle->GetTransform()->Rotation = rots[i];
+				particle->GetTransform()->Scale *= scale;
+			}
+		}
+	}
+}
+
+void DoomCutting::Render()
+{
+}
+
+void DoomCutting::Release()
+{
+	RELEASE(m_CutTimer);
+}
+
+void DoomCutting::OnAlarm(string key)
+{
+}
+
+void DoomCutting::OnCollision(Object* other)
+{
+}
+
+void DoomCutting::OnAnimationEnd(string key)
+{
+}
---
+++ DoomCutting.h
@@ -0,0 +1,20 @@
+#pragma once
+class DoomCutting : public Component
+{
+public:
+	virtual void Init   () override;
+	virtual void Update () override;
+	virtual void Render () override;
+	virtual void Release() override;
+
+	virtual void OnAlarm(string key) override;
+	virtual void OnCollision(Object* other) override;
+	virtual void OnAnimationEnd(string key) override;
+
+private:
+	Timer* m_CutTimer;
+
+	Texture * m_HexagonLight;
+	Renderer* m_Renderer;
+};
+
